1f045f12b93d9d0e52e895c2e9a2470aa0448bd8
shuda <viniciusyashuda008@gmail.com>
2022-11-26 18:22:03 -0300

    Add project files.

---
+++ HigherAndLowerNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HigherAndLowerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double higher = 0, lower = 0, number;
+
+            Console.WriteLine("Enter 15 numbers:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write($"{i + 1}º: ");
+                number = double.Parse(Console.ReadLine());
+
+                if (i == 0)
+                    lower = higher = number;
+                
+                else if (number > higher)
+                    higher = number;
+
+                else if (number < lower)
+                    lower = number;
+            }
+
+            Console.WriteLine("{0} has the higher value, while {1} has the lower", higher, lower);
+
+            Console.ReadKey();
+        }
+    }
+}
